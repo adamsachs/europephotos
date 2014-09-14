77ab461a19a02e98ff38b045e9dff785045f3d0c
Adam Sachs <ajs2255@columbia.edu>
2014-09-14 17:10:48 -0400

    fixing syntax error...

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -55,7 +55,7 @@ $(document).ready(function(){
 		    //event listener for when user clicks on the marker just built--fills the info window with content
 		    //corresponding to that city
 			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.setContent("<a href=\"" + this.gallery + "\"> <img src=\""+ this.image + "\" alt=" " </a>");
+				infowindow.setContent("<a href=\"" + this.gallery + "\"> <img src=\""+ this.image + "\" alt=\" \" </a>");
 				console.log(infowindow.content);	//for testing
 				infowindow.open (map, this);
 
