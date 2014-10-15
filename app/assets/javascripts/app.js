b353531839f83302ee57116494b165a9dae82add
Adam Sachs <ajs2255@columbia.edu>
2014-10-15 19:45:59 -0400

    trying to get firefox infowindow to look better, trying to not use image if firefox browswer

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -154,8 +154,14 @@ $(document).ready(function(){
 		    //event listener for when user clicks on the marker just built--fills the info window with content
 		    //corresponding to that city
 			google.maps.event.addListener(marker, 'click', function() {
+				if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
+					infowindow.setContent("<a id=\"infowindowlink\" href=\"" + this.gallery + "\">" + this.title + "</a>");
+
+				}
+				else{
 				infowindow.setContent("<a id=\"infowindowlink\" href=\"" + this.gallery + "\"> <img src=\""+ this.image + "\" alt=\" \" >" + this.title + "</a>");
-				console.log(infowindow.content);	//for testing
+				}
+				
 				infowindow.open (map, this);
 
 
